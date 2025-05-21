d8478eeb91afff777d9307d5a1e165751812e8e3
ahenla <rymbell@gmail.com>
2025-05-21 18:35:34 +0900

    copilot-cmp

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,12 +1,30 @@
 return {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-        { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-        { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    {
+        "zbirenbaum/copilot.lua", -- or zbirenbaum/copilot.lua
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            })
+        end,
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts ={},
-    keys = {
-        {"<leader>tt", ":CopilotChatToggle<cr>", mode = "n", desc = "toggle chat pannel"}
-    }
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken",                   -- Only on MacOS or Linux
+        opts = {},
+        keys = {
+            { "<leader>tt", ":CopilotChatToggle<cr>", mode = "n", desc = "toggle chat pannel" },
+        },
+    },
 }
